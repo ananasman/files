572e7e8f3b169a5d736a08f2506f77bee7f4815d
AnanasMan <songbl@live.com>
2014-10-09 09:37:20 +0800

    create

---
+++ gen/z/t/files/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package z.t.files;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/z/t/files/FileBrowserActivity.java
@@ -0,0 +1,15 @@
+package z.t.files;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FileBrowserActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_browser);
+	}
+}
